b4ef0e6a3e6f91769937964a79f48384fdc519a7
MiguelEWV <miguelwellspay1@gmail.com>
2021-08-26 14:58:57 -0300

    Parte 3 Completa do do projeto

---
+++ DIO.Project.1.Bank/Classes/Contas.cs
@@ -0,0 +1,81 @@
+using System;
+
+namespace DIO
+{
+    public class Conta
+    {
+//Propiedades        
+        //++++++++++++ Os atributos serao privados para nao seren alterados via codigo so na execucao do programa, isso ajuda fornecer seguranca para dados como o saldo que so e alterado .
+        //Atributo privado string caracteres de nome "Nome" na classe Contas.
+        public Conta(string nome, double saldo, double credito, TipoConta tipoConta)
+        {
+            this.Nome = nome;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.TipoConta = tipoConta;
+
+        }
+        private string Nome { get; set; }
+        //Atributo privado double de numeros decimais exemplo:(1.001) de nome "Saldo" na classe Contas.
+        private double Saldo { get; set; }
+        //Atributo privado double de numeros decimais exemplo:(1.001) de nome "Credito" na classe Contas.
+        private double Credito { get; set; }
+        //Atributo privado 'TipoConta'enum de nome "TipoConta" na classe Contas.
+        //Relembrando Enum e um tipo de arquivo com valores predeterminados nao mutaveis.
+        //Tem o arquivo enum de nome 'TipoConta' com os valores pessoaFisica, entre outros.
+        private TipoConta TipoConta { get; set; }
+
+//Construtor
+        // o metodo construtor da classe publica "Conta" 
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+//Metodos
+        public bool Sacar(double valorSaque)
+        {
+            //Validacao de saldo suficiente
+            if (this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                Console.WriteLine("saldo insuficiente!");
+                return false;
+            }
+                this.Saldo -= valorSaque;
+        //o valor em chave {0} e referente a os dados que estiverem em this.Nome e o valor a ser mostrado na {1} e referente ao this.Saldo
+                Console.WriteLine("saldo atual da conta de {0} e {1}", this.Nome, this.Saldo);
+        //Procurar informacao no docs.microsoft.com composite-formating
+                return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+        // Esta linha faz a mesma funcao <( this.Saldo = this.Saldo + valorDeposito; )>
+            Console.WriteLine("Saldo atual da conta de {0} e {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+//Metodo ToString para represntar via consola a estancia
+        //Metodos ToString tamben pode ser usado para gerar log e saber o que acontece dentro do app
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Credito " + this.Credito;
+            return retorno;
+        }
+
+    }
+}
--- DIO.Project.1.Bank/Program.cs
+++ DIO.Project.1.Bank/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace DIO.Project._1.Bank
-{
+namespace DIO //NameSpace tem que ser o mesmo em todas as classes relacionadas no projeto ou teram que ser chamadas via "using"
+{ 
     class Program
     {
-        static void Main(string[] args)
+      static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Miguel Wells");
+
+            Console.WriteLine(minhaConta.ToString());
             Console.ReadLine();
         }
     }
